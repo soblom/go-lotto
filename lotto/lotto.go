af39fab3e6037532759331e340698d3ef1c37a8b
Sören Blom <soeren.blom@gmail.com>
2015-05-18 00:39:37 +0200

    sorting arrays

--- lotto/lotto.go
+++ lotto/lotto.go
@@ -2,6 +2,7 @@ package lotto
 
 import (
         "fmt"
+        "sort"
 )
 
 type LottoRow struct {
@@ -11,15 +12,23 @@ type LottoRow struct {
 
 type LottoGame []LottoRow
 
+type intArray []int
+func (s intArray) Len() int { return len(s) }
+func (s intArray) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s intArray) Less(i, j int) bool { return s[i] < s[j] }
+
 func EmptyRow() LottoRow {
         return LottoRow{[6]int{},0}
 }
 
-func NewLottoRow(numbers [6]int, super_number int) (result LottoRow, err error) {
+func NewLottoRow(numbers []int, super_number int) (result LottoRow, err error) {
 
         if len(numbers) < 6 {
                 return EmptyRow(),
                         fmt.Errorf("Please specify 6 numbers for each lottery row")
         }
-        return LottoRow{[6]int{},0}, nil
+
+        var sorted_numbers []int = numbers
+
+        return LottoRow{sort.Sort(numbers), super_number}, nil
 }
--- lotto/lotto_test.go
+++ lotto/lotto_test.go
@@ -5,11 +5,20 @@ import (
 _        "fmt"
 )
 
-func TestLottoRow (t *testing.T) {
-        row := NewLottoRow()
-//_        row := lotto.NewLottoRow([]int{1,2,3,4,5,6},7)
-}
+func TestLottoRow(t *testing.T) {
+        in_num := [6]int{6,2,1,4,5,3}
+        in_num_sorted := [6]int{1,2,3,4,5,6}
+        in_super_num := 7
 
-func TestLotteryGame(t *testing.T) {
+        row, err := NewLottoRow(in_num, in_super_num)
 
+        if err != nil {
+                t.Errorf("Creation of valid lotto row (%d,%d) must not create error",
+                        in_num, in_super_num)
+        }
+
+        if row.Numbers != in_num_sorted {
+                t.Errorf("Row (%d) does not match sorted input row (%d)",
+                        row.Numbers, in_num_sorted)
+        }
 }
